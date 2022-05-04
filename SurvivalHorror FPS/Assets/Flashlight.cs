aea33a8714901c04fa701f905ae5158cdfcf61b8
byrocket09 <83572660+AdrianPerez09@users.noreply.github.com>
2022-05-04 17:31:25 +0200

    04/05/2022
    First person character movement, camera and flashlight

---
+++ SurvivalHorror FPS/Assets/Flashlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight : MonoBehaviour
+{
+    [SerializeField] GameObject spotlight;
+    private bool flashlightActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spotlight.gameObject.SetActive(false);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!flashlightActive)
+            {
+                spotlight.gameObject.SetActive(true);
+                flashlightActive = true;
+            }
+            else
+            {
+                spotlight.gameObject.SetActive(false);
+                flashlightActive = false;
+            }
+        }       
+    }
+}
